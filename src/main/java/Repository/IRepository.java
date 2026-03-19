fff62fda1c5fbd8264e37c5f484514a523600e6d
Zahrah Vermaak <221406395@mycput.ac.za>
2026-03-19 13:15:39 +0200

    Created IRepository interface and added necessary code

---
+++ src/main/java/Repository/IRepository.java
@@ -0,0 +1,10 @@
+package Repository;
+
+public interface IRepository <T, ID>  {
+
+        T create(T entity);
+        T read(ID id);
+        T update(T entity);
+        boolean delete(ID id);
+
+}
